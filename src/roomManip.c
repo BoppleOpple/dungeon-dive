ccd670adcd1f08a9bc98d3df3665a2b43ee7bff7
BoppleOpple <boppleopple@gmail.com>
2024-10-30 01:36:33 -0400

    i have suffered gdb for this, but now theres a list struct :/

--- src/roomManip.c
+++ src/roomManip.c
@@ -25,13 +25,53 @@ ROOM *roomCreate(ROOM *room) {
 	return newRoom;
 }
 
+ROOM_LIST roomListCreate() {
+	ROOM_LIST list = { malloc(sizeof(ROOM *)), ROOM_ARRAY_SIZE, 0 };
+	*list.array = malloc(sizeof(ROOM) * ROOM_ARRAY_SIZE);
+	return list;
+}
+
+
+void roomListAppendRoom(ROOM_LIST *list, ROOM *room) {
+	list->size++;
+	
+	// if the current room is oob, add more rooms
+	if (list->size >= list->maxSize) {
+		// double the size of the array
+		list->maxSize *= 2;
+		// and safely reallocate its space
+		ROOM *newArray = realloc(*list->array, list->maxSize*sizeof(ROOM));
+		if (!newArray) {
+			free(*list->array);
+			printf("could not allocate memory somehow");
+			exit(1);
+			return;
+		}
+		*list->array = newArray;
+	}
+
+	if (room != NULL)
+		*(list->array + list->size - 1) = room;
+	else {
+		ROOM *newRoom = *(list->array) + list->size - 1;
+		newRoom->roomCode = "";
+		newRoom->name = "";
+		newRoom->description = "";
+		newRoom->north = NULL;
+		newRoom->south = NULL;
+		newRoom->east = NULL;
+		newRoom->west = NULL;
+	}
+}
+
+ROOM *roomListGetElement(ROOM_LIST *list, int i) {
+	return *(list->array) + i;
+}
+
 ROOM *readRoomFile(const char *filepath, int *arraySize) {
 	// forward declare a bunch of variables
 	FILE *roomFile;
-	int maxRooms = ROOM_ARRAY_SIZE; // maximum size of the array
-	*arraySize = 0; // current size of the array
-	int incrementRoom = 0; // bool for whether or not to increment the room in the loop (I know bools exist, but I dont mind ints)
-	ROOM *roomArray = malloc(sizeof(ROOM)*maxRooms); // allocate enough space for the current maximum number of rooms
+	ROOM_LIST roomList = roomListCreate(); // allocate enough space for the current maximum number of rooms
 	/*
 	currentField points to a pointer, so this allocates the space needed for the line data to the
 	character pointer in the Room structure.
@@ -50,7 +90,7 @@ ROOM *readRoomFile(const char *filepath, int *arraySize) {
 	would.
 	*/
 	char **currentField;
-
+	
 	char lineBuffer[1024]; // allocate some space for reading lines from roomFile
 	char *lineData = lineBuffer; // this will contain only the "important" parts of the line (so not the prefix)
 	char *prefix; // when reading lines, this is the c string determining which field the text falls into
@@ -63,21 +103,18 @@ ROOM *readRoomFile(const char *filepath, int *arraySize) {
 
 	// iterate through each line of the file
 	while (fgets(lineBuffer, sizeof(lineBuffer), roomFile)) {
-		
 
 		// set the prefix and c string pointer depending on the starting text
-		if (strncmp(lineBuffer, ROOM_CODE, strlen(ROOM_CODE)) == 0) {
-			prefix = ROOM_CODE;
-			currentField = &(roomArray + *arraySize)->roomCode;
-		} else if (strncmp(lineBuffer, ROOM_NAME, strlen(ROOM_NAME)) == 0) {
+		if (strncmp(lineBuffer, ROOM_NAME, strlen(ROOM_NAME)) == 0) {
+			roomListAppendRoom(&roomList, NULL);
 			prefix = ROOM_NAME;
-			currentField = &(roomArray + *arraySize)->name;
+			currentField = &roomListGetElement(&roomList, roomList.size - 1)->name;
+		} else if (strncmp(lineBuffer, ROOM_CODE, strlen(ROOM_CODE)) == 0) {
+			prefix = ROOM_CODE;
+			currentField = &roomListGetElement(&roomList, roomList.size - 1)->roomCode;
 		} else if (strncmp(lineBuffer, ROOM_DESC, strlen(ROOM_DESC)) == 0) {
 			prefix = ROOM_DESC;
-			currentField = &(roomArray + *arraySize)->description;
-
-			// since description is the last one passed, move to the next room once it's read
-			incrementRoom = 1;
+			currentField = &roomListGetElement(&roomList, roomList.size - 1)->description;
 		} else {
 			// if it is unknown, there's nothing to do here
 			continue;
@@ -85,43 +122,17 @@ ROOM *readRoomFile(const char *filepath, int *arraySize) {
 
 		// select only the content after the prefix (description/name/code specifier) and trim whitespace
 		lineData = str_trim(lineBuffer + strlen(prefix));
-
+		printf("%i %s|---|%s\n", roomList.size, prefix, lineBuffer);
 		// now, do that weird thing.
 		*currentField = malloc( sizeof(char) * ( strlen(lineData) + 1 ) );
 		strcpy(*currentField, lineData);
+		printf("%s|---|%s\n", prefix, *currentField);
 		// this allocates exactly as much memory as is needed to hold all the strings, and doesnt set limits
 		// on the length of names, codes, or descriptions. whether or not this is good is in the eye of the
 		// beholder, but I think it is (it was also my first thought before you statically allocated space
 		// in class) :P
-
-		if (incrementRoom) {
-			incrementRoom = 0;
-
-			// debug output, mostly just muddies up the terminal
-			// printf("Room %s: %s\n%s\n\n", (roomArray + *arraySize)->roomCode, (roomArray + *arraySize)->name, (roomArray + *arraySize)->description);
-
-			(*arraySize)++;
-
-			// if the current room is oob, add more rooms
-			if (*arraySize >= maxRooms) {
-				// debug output, might as well keep it around
-				// printf("current size: %li\n", maxRooms*sizeof(Room));
-				// printf("new size: %li\n", maxRooms*sizeof(Room)*2);
-
-				// double the size of the array
-				maxRooms *= 2;
-
-				// and safely reallocate its space
-				ROOM *newRoomArray = realloc(roomArray, maxRooms*sizeof(ROOM));
-				if (!newRoomArray) {
-					free(roomArray);
-					printf("could not allocate memory somehow");
-					return NULL;
-				}
-				roomArray = newRoomArray;
-			}
-		}
 	}
 
-	return roomArray;
+	*arraySize = roomList.size;
+	return *(roomList.array);
 }
--- src/roomManip.h
+++ src/roomManip.h
@@ -12,13 +12,33 @@ struct ROOM {
 	ROOM *west;
 };
 
+typedef struct {
+	ROOM **array;
+	int maxSize;
+	int size;
+}ROOM_LIST;
+
 #ifndef ROOM_MANIP_CREATE
 #define ROOM_MANIP_CREATE
 ROOM *roomCreate(ROOM *room);
 #endif
 
-
 #ifndef ROOM_MANIP_READ_FILE
 #define ROOM_MANIP_READ_FILE
 ROOM *readRoomFile(const char *filepath, int *arraySize);
+#endif
+
+#ifndef ROOM_MANIP_CREATE_LIST
+#define ROOM_MANIP_CREATE_LIST
+ROOM_LIST roomListCreate();
+#endif
+
+#ifndef ROOM_MANIP_LIST_APPEND
+#define ROOM_MANIP_LIST_APPEND
+void roomListAppendRoom(ROOM_LIST *list, ROOM *room);
+#endif
+
+#ifndef ROOM_MANIP_LIST_GET
+#define ROOM_MANIP_LIST_GET
+ROOM *roomListGetElement(ROOM_LIST *list, int i);
 #endif
